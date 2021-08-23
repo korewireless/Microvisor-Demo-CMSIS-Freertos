4e834445d4d786f9591437055c7a7acf97cc8ccd
Phil Michaelson-Yeates <pmichaelsonyeates@twilio.com>
2021-08-23 12:07:45 +0100

    replace mvTemporaryServerLog with calls to channel API

--- Demo/Src/main.c
+++ Demo/Src/main.c
@@ -59,6 +59,11 @@ const osThreadAttr_t DebugTask_attributes = {
   .priority = (osPriority_t) osPriorityNormal,
   .stack_size = 1024
 };
+struct {
+    MvNotificationHandle notification;
+    MvNetworkHandle network;
+    MvChannelHandle channel;
+} log_handles = { 0, 0, 0 };
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -68,6 +73,9 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 void StartGPIOTask(void *argument);
 void StartDebugTask(void *argument);
+void OpenLogChannel();
+void CloseLogChannel();
+void ServerLog(const char *str);
 
 /* USER CODE BEGIN PFP */
 
@@ -75,6 +83,90 @@ void StartDebugTask(void *argument);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void OpenLogChannel()
+{
+    static volatile struct MvNotification log_notification_buffer[16];
+    memset((void *)log_notification_buffer, 0xff, sizeof(log_notification_buffer));
+    static struct MvNotificationSetup setup = {
+        .irq = TIM8_BRK_IRQn,
+        .buffer = (struct MvNotification *)log_notification_buffer,
+        .buffer_size = sizeof(log_notification_buffer)
+    };
+    uint32_t status = mvSetupNotifications(&setup, &log_handles.notification);
+    assert(status == MV_STATUS_OKAY);
+
+    struct MvRequestNetworkParams nw_params = {
+        .version = 1,
+        .v1 = {
+            .notification_handle = log_handles.notification,
+            .notification_tag = 123
+        }
+    };
+    status = mvRequestNetwork(&nw_params, &log_handles.network);
+    assert(status == MV_STATUS_OKAY);
+
+    static volatile uint8_t receive_buffer[16];
+    static volatile uint8_t send_buffer[512] __attribute__((aligned(512)));
+    uint8_t endpoint[] = { 'l', 'o', 'g' };
+    struct MvOpenChannelParams ch_params = {
+        .version = 1,
+        .v1 = {
+            .notification_handle = log_handles.notification,
+            .notification_tag = 456,
+            .network_handle = log_handles.network,
+            .receive_buffer = (uint8_t*)receive_buffer,
+            .receive_buffer_len = sizeof(receive_buffer),
+            .send_buffer = (uint8_t*)send_buffer,
+            .send_buffer_len = sizeof(send_buffer),
+            .channel_type = 0,
+            .endpoint = endpoint,
+            .endpoint_len = sizeof(endpoint)
+        }
+    };
+
+    while (1) {  // busy wait for connection
+        enum MvNetworkStatus status;
+        if (mvGetNetworkStatus(log_handles.network, &status) == MV_STATUS_OKAY &&
+            status == MV_NETWORKSTATUS_CONNECTED) {
+            break;
+        }
+        for (volatile unsigned i = 0; i < 50000; i++) {} // busy delay
+    }
+
+    status = mvOpenChannel(&ch_params, &log_handles.channel);
+    assert(status == MV_STATUS_OKAY);
+}
+
+void CloseLogChannel()
+{
+    if (log_handles.channel != 0) {
+      uint32_t status = mvCloseChannel(&log_handles.channel);
+      assert(status == MV_STATUS_OKAY);
+    }
+    assert(log_handles.channel == 0);
+    if (log_handles.network != 0) {
+      uint32_t status = mvReleaseNetwork(&log_handles.network);
+      assert(status == MV_STATUS_OKAY);
+    }
+    assert(log_handles.network == 0);
+    if (log_handles.notification != 0) {
+      uint32_t status = mvCloseNotifications(&log_handles.notification);
+      assert(status == MV_STATUS_OKAY);
+    }
+    assert(log_handles.notification == 0);
+}
+
+void ServerLog(const char *str)
+{
+    if (log_handles.channel == 0) {
+        OpenLogChannel();
+    }
+    uint32_t available, status;
+    status = mvWriteChannel(log_handles.channel, (const uint8_t*)str, strlen(str), &available);
+    assert(status == MV_STATUS_OKAY);
+    status = mvWriteChannel(log_handles.channel, (const uint8_t*)"\n", 1, &available);
+    assert(status == MV_STATUS_OKAY);
+}
 /* USER CODE END 0 */
 
 /**
@@ -92,7 +184,8 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-  mvTemporaryServerLog((uint8_t *)"hello world", 11);
+  ServerLog("hello world");
+  CloseLogChannel();
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -230,8 +323,8 @@ void StartDebugTask(void *argument)
   /* Infinite loop */
   for(;;)
   {
-    sprintf(buffer, "ping %u", n);
-    mvTemporaryServerLog((uint8_t *)buffer, strlen(buffer));
+    snprintf(buffer, sizeof(buffer), "ping %u", n);
+    ServerLog(buffer);
     n++;
     osDelay(1000);
   }
